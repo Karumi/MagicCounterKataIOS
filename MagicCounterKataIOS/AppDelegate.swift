07be93e2f8203a45a0699b56b208d9069f1c1c03
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2019-11-29 13:25:11 +0100

    Prepare an empty project with the linter and the travis configuration

---
+++ MagicCounterKataIOS/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  MagicCounterKataIOS
+//
+//  Created by Pedro Gómez on 29/11/2019.
+//  Copyright © 2019 Karumi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication,
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+
+}
+
---
+++ MagicCounterKataIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicCounterKataIOS
+//
+//  Created by Pedro Gómez on 29/11/2019.
+//  Copyright © 2019 Karumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
